222176d7f534c3042303ce735d36a4a6dfbc05b9
ApisitKasem <mr.apisit.kasem@gmail.com>
2025-05-19 10:26:28 +0700

    initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {PluginComponent} from './pages/plugin/plugin/plugin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PluginComponent }
+];
---
+++ src/app/pages/plugin/plugin/plugin.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-plugin',
+  imports: [],
+  templateUrl: './plugin.component.html',
+  styleUrl: './plugin.component.css',
+  standalone: true
+})
+export class PluginComponent {
+
+}
